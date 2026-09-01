52fa0eb6deb7651a92ae7f8e51bf9b20a5460d20
Niko Savola <nikomsavola@gmail.com>
2026-09-01 15:11:09 +0300

    Bump version to 0.1.1 (#4)
    versionCode 1 -> 2, versionName 0.1.0 -> 0.1.1.

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -59,8 +59,8 @@ android {
     applicationId = "fi.nikosavola.clockifywear"
     minSdk = 30
     targetSdk = 36
-    versionCode = 1
-    versionName = "0.1.0"
+    versionCode = 2
+    versionName = "0.1.1"
   }
 
   signingConfigs {
